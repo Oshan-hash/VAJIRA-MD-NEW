df646f880c93c4e5a9a47d16aad7c66012917a92
Oshan-hash <oshanashinsa2@gmail.com>
2024-09-27 19:16:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=fQ9V1IKY#dGYduv9H8DufMrJShuynS6BRxFkBKa0jTFsOs9pS0EM' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=yJE0DAhC#cckVkl7rnKQxI1MWa1fhYwktHR2or7nI89lfYgUEB60' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
